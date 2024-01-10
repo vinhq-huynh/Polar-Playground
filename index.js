11fbce80c632221b05cee4801110f6f1cac75b33
Vinh Huynh <vinh-huynh@outlook.com>
2024-01-09 22:45:53 -0800

    Add Express.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
- 
+import express from 'express';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 9000;
+
+app.use(express.static('public'))
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
