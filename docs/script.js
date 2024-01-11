038285755ddd7febf660f2c556aa345a952b38ff
Vinh Huynh <vinh-huynh@outlook.com>
2024-01-10 16:51:19 -0800

    Change publish source to mobile-first/docs

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,10 @@
 const navbar_container = document.querySelector('.nav-bar-container');
 
+// Handling click hambuger menu bar
+document.querySelector('.fa-bars').addEventListener('click', () => {
+    console.log("YO");
+});
+
 window.addEventListener('scroll', () => {
     // if (window.scrollY > 0) {
     //     bg1.classList.add('bg-1-scrolled');
