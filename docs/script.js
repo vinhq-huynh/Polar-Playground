f297d73e1183e91e3cc8ccacb3f48cd6b8202ef2
Vinh Huynh <vinh-huynh@outlook.com>
2024-01-11 00:43:49 -0800

    Add hamburger menu for mobile

--- docs/script.js
+++ docs/script.js
@@ -1,8 +1,10 @@
 const navbar_container = document.querySelector('.nav-bar-container');
+const navlink_container = document.querySelector('.nav-link-container');
 
 // Handling click hambuger menu bar
 document.querySelector('.fa-bars').addEventListener('click', () => {
-    console.log("YO");
+    navbar_container.classList.toggle('nav-bar-dark');
+   navlink_container.classList.toggle('d-none');
 });
 
 window.addEventListener('scroll', () => {
@@ -14,7 +16,9 @@ window.addEventListener('scroll', () => {
 
     if (window.scrollY > 30) {
         navbar_container.classList.add('nav-bar-scrolled');
+        navlink_container.style.top = "3.5rem";
     } else if (window.scrollY <= 30) {
         navbar_container.classList.remove('nav-bar-scrolled');
+        navlink_container.style.top = "5rem";
     }
 })
