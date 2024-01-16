5e0577cb69b940b4c9e80bbd5c549bb054744bfe
Vinh Huynh <vinh-huynh@outlook.com>
2024-01-15 20:16:37 -0800

    Add more characters images and menu page.

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-import express from 'express';
-import { dirname } from "path";
+import fs from "fs"
+import express from "express";
+import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
-const port = 80;
+const port = 8000;
 
 app.use(express.static('public'))
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
+})
+
+app.get("/menu", (req, res) => {
+    // Get all characters names
+    fs.readdir(__dirname + "/public/assets/images/characters", (err, files) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render(__dirname + "/public/menu.ejs", { all_characters : files, __dirname : __dirname });
+        }
+    })
 })
 
 app.listen(port, () => {
