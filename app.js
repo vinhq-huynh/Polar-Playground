4cd31f15faa0cd47cfae62e1294f38f4f1a7ab81
Vinh Huynh <vinh-huynh@outlook.com>
2024-01-16 21:23:57 -0800

    Major refactor. All pages are now EJS. Overall restructure of folders

---
+++ app.js
@@ -0,0 +1,61 @@
+import fs from "fs"
+import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 9000;
+
+app.use(express.static('public'));
+
+app.set("view engine", "ejs");
+app.set('views', __dirname + '/views');
+
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+app.get("/menu", (req, res) => {
+    // Get all characters names
+    fs.readdir(path.join(__dirname, "public", "assets", "images", "characters"), (err, files) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("menu", { all_characters : files, __dirname : __dirname });
+        }
+    })
+})
+
+app.get("/hours", (req, res) => {
+    res.render("hours");
+})
+
+app.get("/about", (req, res) => {
+    res.render("about");
+})
+
+const waittime_phrases = [
+    "we are not busy at the moment.",
+    "we are a little busy right now.",
+    "we are quite busy right now.",
+    "we are really busy at the moment.",
+    "we are experiencing the highest demands right now.",
+    "we are closed for today.",
+    "we are closed today."
+]
+app.get("/waittime", (req, res) => {
+    // Get waittime from server
+    
+    // Build waittime object
+    var waittime = {
+        hour: 0,
+        minute: 0,
+        updatedTime: 0
+    }
+    res.render("waittime", { phrases : waittime_phrases, waittime : waittime });
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,18 +1,19 @@
 const navbar_container = document.querySelector('.nav-bar-container');
-const navlink_container = document.querySelector('.nav-link-container');
+const nav_links = document.querySelector('.nav-links');
 
 // Handling click hambuger menu bar
 document.querySelector('.fa-bars').addEventListener('click', () => {
+
     navbar_container.classList.toggle('nav-bar-dark');
-    navlink_container.classList.toggle('d-none');
+    nav_links.classList.toggle('d-none');
 });
 
 window.addEventListener('scroll', () => {
     if (window.scrollY > 30) {
         navbar_container.classList.add('nav-bar-scrolled');
-        navlink_container.style.top = "3.5rem";
+        nav_links.style.top = "3.5rem";
     } else if (window.scrollY <= 30) {
         navbar_container.classList.remove('nav-bar-scrolled');
-        navlink_container.style.top = "5rem";
+        nav_links.style.top = "5rem";
     }
 })
