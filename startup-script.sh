fdbe2ed29fc896c1209c1b8861be9d82739910c5
Vinh Huynh <vinh-huynh@outlook.com>
2024-01-17 14:36:49 -0800

    Update startup script

--- startup-script.sh
+++ startup-script.sh
@@ -1,44 +1,20 @@
 #!/bin/bash
 
-# Install logging monitor. The monitor will automatically pick up logs sent to
+# Install Ops Agent. The monitor will automatically pick up logs sent to
 # syslog.
-curl -s "https://storage.googleapis.com/signals-agents/logging/google-fluentd-install.sh" | bash
-service google-fluentd restart &
+curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 
 # Install dependencies from apt
 apt-get update
-apt-get install -yq ca-certificates git build-essential supervisor psmisc
-
-# Install nodejs
-mkdir /opt/nodejs
-curl https://nodejs.org/dist/v20.11.0/node-v20.11.0-linux-x64.tar.xz | tar xvzf - -C /opt/nodejs --strip-components=1
-ln -s /opt/nodejs/bin/node /usr/bin/node
-ln -s /opt/nodejs/bin/npm /usr/bin/npm
+apt-get install -yq ca-certificates git build-essential nodejs npm
 
 # Get the application source code from the Google Cloud Storage bucket.
-mkdir /Polar-Playground
-gsutil -m cp -r gs://polar-playground-src-bucket/Polar-Playground
+gsutil -m cp -r gs://polar-playground-src-bucket/Polar-Playground /
 
 # Install app dependencies.
 cd /Polar-Playground
 npm install
 
-# Create a nodeapp user. The application will run as this user.
-useradd -m -d /home/nodeapp nodeapp
-chown -R nodeapp:nodeapp /opt/app
-
-# Configure supervisor to run the node app.
-cat >/etc/supervisor/conf.d/node-app.conf << EOF
-[program:nodeapp]
-directory=/Polar-Playground
-command=npm start
-autostart=true
-autorestart=true
-user=nodeapp
-environment=HOME="/home/nodeapp",USER="nodeapp",NODE_ENV="production"
-stdout_logfile=syslog
-stderr_logfile=syslog
-EOF
-
-supervisorctl reread
-supervisorctl update
+# Run the app
+npm start
